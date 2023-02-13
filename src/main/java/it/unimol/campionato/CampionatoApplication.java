e4ee17c345a5f09e96947141abec13228aa0e1d7
Mattiadv02 <m.devincenzo@studenti.unimol.it>
2023-02-13 09:49:11 +0100

    primo commit

---
+++ src/main/java/it/unimol/campionato/CampionatoApplication.java
@@ -0,0 +1,13 @@
+package it.unimol.campionato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampionatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampionatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unimol/campionato/CampionatoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimol.campionato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampionatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
